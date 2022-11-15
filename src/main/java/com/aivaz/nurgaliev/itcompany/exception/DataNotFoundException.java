8cb7f4df21b4dfde82f156a633163dd302149627
AivazNurgaliev <aivaznurgaliev@gmail.com>
2022-11-15 22:07:16 +0300

    updated: datanotfoundexception

--- src/main/java/com/aivaz/nurgaliev/itcompany/exception/DataNotFoundException.java
+++ src/main/java/com/aivaz/nurgaliev/itcompany/exception/DataNotFoundException.java
@@ -4,4 +4,12 @@ public class DataNotFoundException extends Exception {
     public DataNotFoundException(String msg) {
         super(msg);
     }
+
+    public DataNotFoundException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public DataNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
